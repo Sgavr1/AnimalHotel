da60e9db238078a89f10c4d276b7a308049bbe82
Sgavr1 <sanya.gavrilko60@gmail.com>
2022-10-11 22:44:43 +0300

    Хуета трошки в админе сделал

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -3,14 +3,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AnimalHotel.Models;
 
 namespace AnimalHotel.Controllers
 {
     public class AdminController : Controller
     {
+        string postgreas = "admin";
+        MessageModel message = null;
+        AccountModel account = new AccountModel();
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult ToLogin(int id)
+        {
+            account.GetAccountById(id, postgreas);
+
+            return Redirect("/Admin");
         }
     }
 }
